765c364c1ed9e5e1f31d950b40d0ca8342abf31d
Ajay Wankhade <ajaysw45@gmail.com>
2025-03-11 19:04:40 +0530

    frontend removed

--- frontend/src/pages/editScores.jsx
+++
@@ -1,9 +0,0 @@
-﻿const EditScores = () => {
-    return (
-        <div>
-            <h1>Edit Scores</h1>
-        </div>
-    )
-}
-
-export default EditScores;
--- frontend/src/pages/schedule.jsx
+++
@@ -1,9 +0,0 @@
-﻿const Schedule = () => {
-    return (
-        <div>
-            <h1>Schedule</h1>
-        </div>
-    )
-}
-
-export default Schedule;
