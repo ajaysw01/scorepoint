cd96a8a04dff94dcbbad2e2d9f1c49b602fe76a6
Ajay Wankhade <ajaysw45@gmail.com>
2025-03-20 18:59:51 +0530

    added cursor pointers to buttons

--- frontend/src/pages/updates.jsx
+++ frontend/src/pages/updates.jsx
@@ -88,7 +88,7 @@ const Updates = () => {
               <button
                 key={category}
                 onClick={() => setCurrentCategory(category)}
-                className={`whitespace-nowrap px-4 py-2 text-sm font-medium rounded-lg transition-all  ${
+                className={`whitespace-nowrap px-4 py-2 text-sm font-medium rounded-lg transition-all cursor-pointer  ${
                   currentCategory === category
                     ? "bg-red-500 text-white"
                     : "bg-gray-200 text-gray-700"
